674bb3a3f2679d86901a3a40924ba060d1930041
warsaiqbal <160345806+warsaiqbal@users.noreply.github.com>
2024-06-05 13:26:48 -0700

    Add files via upload

---
+++ 28. Favorite Fruit/main.ts
@@ -0,0 +1,13 @@
+let favourite_fruits: string[] = ["Mangoes", "Strawberries", "Blueberries", "Apples", "Grapes"] 
+
+if (favourite_fruits.includes("Mangoes")){
+    console.log("I like to eat mangoes in summers");
+} if (favourite_fruits.includes("Blueberries")){
+    console.log("I would love to try Blueberries");
+} if (favourite_fruits.includes("Strawberries")){
+    console.log("Strawberries are good for your health");
+} if (favourite_fruits.includes("Apples")){
+    console.log("Apples are good for digestion");
+} if (favourite_fruits.includes("Grapes")){
+    console.log("Grapes are good for eyes");
+}
